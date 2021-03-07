b6c4637e4384fa5cc8471c6c00924555f7629006
hisui0 <lassloshbf@gmail.com>
2021-03-08 03:41:05 +0900

    config.storageをcredentialの後に追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,6 @@
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 #
  CarrierWave.configure do |config|
-   config.storage = :fog
    config.fog_provider = 'fog/aws'                                             # required
    config.fog_credentials = {
      provider:              'AWS',                                             # required
@@ -22,6 +21,7 @@
 #     host:                  's3.example.com',                                  # optional, defaults to nil
 #     endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
    }
+   config.storage = :fog
    config.fog_directory  = 'reiwacity-decidim-bucket'                                 # required
 #   config.fog_public     = false                                               # optional, defaults to true
 #   config.fog_attributes = {
